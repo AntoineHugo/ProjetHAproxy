53925be97244a86a41ac41fb479c090af7ea569c
AntoineHugo <72555913+AntoineHugo@users.noreply.github.com>
2021-03-19 10:15:40 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -209,6 +209,6 @@ echo ""
 echo ""
 echo -e "${GREEN}${BOLD}Le script s'est terminé.${NORMAL}"
 echo ""
-echo -e "${GREEN}${BOLD}Vous pouvez accéder à l'URL http://"${IP:0:14}" pour se connecter aux serveurs WEB et http://"${haproxysupervision// /}" pour l'interface Haproxy (user :${NORMAL} ${BOLD}administration ${GREEN}${BOLD}/ mot de passe :${NORMAL} ${BOLD}passroot${NORMAL}"
+echo -e "${GREEN}${BOLD}Vous pouvez accéder à l'URL${NORMAL} ${BOLD}http://"${IP:0:14}" ${GREEN}${BOLD}pour se connecter aux serveurs WEB et${NORMAL} ${BOLD}http://"${haproxysupervision// /}" ${GREEN}${BOLD}pour l'interface Haproxy (user :${NORMAL} ${BOLD}administration ${GREEN}${BOLD}/ mot de passe :${NORMAL} ${BOLD}passroot${NORMAL}"
 echo ""
 fi
